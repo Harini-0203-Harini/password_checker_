dd8b25f40ec6d53a786172160f247eede95432c9
Harini-0203-Harini <814724104046@trp.srmtrichy.edu.in>
2026-07-17 22:22:31 +0530

    Add files via upload

---
+++ password checker.py
@@ -0,0 +1,18 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/manih/AppData/Local/Programs/Python/Python314/password checker.py
+Enter your password: harini@123
+Medium Password
+>>> 
+= RESTART: C:/Users/manih/AppData/Local/Programs/Python/Python314/password checker.py
+Enter your password: Harini@cyber
+Medium Password
+>>> 
+= RESTART: C:/Users/manih/AppData/Local/Programs/Python/Python314/password checker.py
+Enter your password: Harini@123
+Strong Password
+>>> 
+= RESTART: C:/Users/manih/AppData/Local/Programs/Python/Python314/password checker.py
+Enter your password: 123
+Weak Password
